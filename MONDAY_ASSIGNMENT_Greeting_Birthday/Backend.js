db61c9d45c64afa8704d024e48dc6274c2b04638
Vikrant kumar <42002633+vikrant5432@users.noreply.github.com>
2022-03-02 13:25:58 +0530

    Add files via upload

--- MONDAY_ASSIGNMENT_Greeting_Birthday/Backend.js
+++ MONDAY_ASSIGNMENT_Greeting_Birthday/Backend.js
@@ -2,14 +2,11 @@ const COLOR_SPACE = "black";
 const COLOR_STARS = "white";
 const STAR_NUM = 500;
 const STAR_SIZE = 0.005;
-const STAR_SPEED = 0.05;
 
 var canvas = document.getElementById("Canvas");
 var ctx = canvas.getContext("2d");
 canvas.height = 770;
 canvas.width = 800;
-
-
 
 function create_star(i, j) {
   ctx.beginPath();
@@ -30,82 +27,30 @@ function create_star(i, j) {
   ctx.stroke();
 }
 
-
-
-//Practice with external resource help
-//star ball
-var stars = [];
-var starSpeed = STAR_SPEED * canvas.width;
-var xv = starSpeed * randomSign() * Math.random();
-// Using Pythagoras' theorem, yv = sqrt(starSpeed^2 - xv^2)
-var yv = Math.sqrt(Math.pow(starSpeed, 2) - Math.pow(xv, 2)) * randomSign();
-for (let i = 0; i < STAR_NUM; i++) {
-  let speedMult = Math.random() * 2 + 0.5;
-  stars[i] = {
-    r: (Math.random() * STAR_SIZE * canvas.width) / 2,
-    x: Math.floor(Math.random() * canvas.width),
-    y: Math.floor(Math.random() * canvas.height),
-    xv: xv * speedMult,
-    yv: yv * speedMult,
-  };
-}
-
-
-//loops start
-var timeDelta,
-  timeLast = 0;
-
-function loop(timeNow) {
-  timeDelta = timeNow - timeLast;
-  timeLast = timeNow;
-
-  // space background
+function Greet_loop() {
   ctx.fillStyle = COLOR_SPACE;
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  // draw the stars
   ctx.fillStyle = COLOR_STARS;
-  for (let i = 0; i < STAR_NUM; i++) {
+  for (let i = 0; i < 200; i++) {
     ctx.beginPath();
-    ctx.arc(stars[i].x, stars[i].y, stars[i].r, 0, Math.PI * 2);
+    ctx.arc(
+      Math.random() * canvas.width,
+      Math.random() * canvas.height,
+      (Math.random() * STAR_SIZE * canvas.width) / 2,
+      0,
+      Math.PI * 2
+    );
     ctx.fill();
-    
-    //x-update
-    stars[i].x += stars[i].xv * timeDelta * 0.001;
-
-    // changing the position of stars to the other side if it goes off screen
-    if (stars[i].x < 0 - stars[i].r) {
-      stars[i].x = canvas.width + stars[i].r;
-    } else if (stars[i].x > canvas.width + stars[i].r) {
-      stars[i].x = 0 - stars[i].r;
-    }
-    
-    //y-update
-    stars[i].y += stars[i].yv * timeDelta * 0.001;
-
-    //changing the position of the stars to the other side if it goes off screen
-    if (stars[i].y < 0 - stars[i].r) {
-      stars[i].y = canvas.height + stars[i].r;
-    } else if (stars[i].y > canvas.height + stars[i].r) {
-      stars[i].y = 0 - stars[i].r;
-    }
   }
-  Message();
   create_star(100, 0);
   create_star(0, 500);
   create_star(600, 100);
   create_star(700, 600);
   create_star(300, 700);
-
-  requestAnimationFrame(loop);
-}
-
-function randomSign() {
-  return Math.random() >= 0.5 ? 1 : -1;
 }
 
 function Message() {
-  // Greet_loop();
   var name = document.getElementById("name").value;
   var birthday_date = document.getElementById("birthday_date").valueAsNumber;
 
@@ -122,5 +67,7 @@ function Message() {
 }
 
 function Card() {
-  requestAnimationFrame(loop);
+  Greet_loop();
+  Message();
+  ctx.stroke();
 }
