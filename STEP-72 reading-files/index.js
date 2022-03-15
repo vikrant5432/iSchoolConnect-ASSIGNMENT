7e2cdad3a6fed6ae3c91f34fe5b0859fa2368416
Vikrant kumar <42002633+vikrant5432@users.noreply.github.com>
2022-03-16 00:35:22 +0530

    Delete STEP-72 reading-files directory

--- STEP-72 reading-files/index.js
+++
@@ -1,9 +0,0 @@
-const fetch = require("fetch");
-const fs = require("fs");
-fetch.fetchUrl("https://arcstellar.com", (error, meta, data) => {
-  if (error) {
-    console.log("Eorror:- ", error);
-  } else {
-    fs.writeFileSync("tempfile/arctellar.html", data, "utf-8");
-  }
-});
